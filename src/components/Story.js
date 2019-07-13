f276a3ae6429c27feab3c28cb04d4e8b80abd5f3
jaypardel3 <jaypardelobate101994@gmail.com>
2019-07-13 23:58:35 +0800

    adding story section

---
+++ src/components/Story.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Story.scss';
+
+const Home = () => {
+    return (
+        <Container fluid={true} className='story-img'>
+            <Row>
+                <Col></Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Home;
